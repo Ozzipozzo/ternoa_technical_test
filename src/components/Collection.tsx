affa512e05c61068a7e4397a886efff045b6bd3a
Sleazy Gram <abdallah.ghanmi>
2023-05-07 14:55:03 +0200

    [Feat] Header and main home done

---
+++ src/components/Collection.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+
+const Collection: React.FC = () => {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+};
+
+export default Collection;
